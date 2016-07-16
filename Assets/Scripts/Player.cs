565f345178e42898c60989891ddbd1af50474600
josephquested <josephquested@gmail.com>
2016-07-17 10:36:45 +1200

    add health drain

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	Transform moon;
+
+	public float health;
+
+	void Start ()
+	{
+		moon = GameObject.FindWithTag("Moon").transform;
+	}
+
+	void Update ()
+	{
+		RaycastHit hitInfo;
+
+		if (Physics.Linecast(transform.position, moon.position, out hitInfo))
+		{
+			if (hitInfo.collider.tag == "Moon")
+			{
+				print("in moon!");
+				health -= 0.1f;
+			}
+		}
+
+		if (health <= 0)
+		{
+			print("dead!");
+		}
+	}
+}
